830e556d69fccf7ec78fb41dd5b1a905a7a977f4
rishi aluri <rishialuri@gmail.com>
2020-12-15 18:49:44 +0530

    Adding Navigation

---
+++ src/components/Logo/logo.js
@@ -0,0 +1,12 @@
+import Tilt from 'react-tilt';
+import './logo.css';
+import logoww from './walterwhite.png';
+
+const Logo = () =>{
+    return(
+        <Tilt className="Tilt pa3 shadow-3 br3 ma3" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+         <div className="Tilt-inner tc"><img src={logoww} alt='logo'/></div>
+        </Tilt>
+    );
+}
+export default Logo;
---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,11 @@
+const Navigation = () => {
+    return(
+        <nav>
+            <p className='pa3 link black underline pointer' 
+            style={{display:'flex', justifyContent:'flex-end'}}>
+                Sign out!!
+            </p>
+        </nav>
+    );
+}
+export default Navigation;
